f2efe2b1801ec720735b5582678de20a1df2043c
nyg7 <yuricantox@gmail.com>
2026-05-06 16:20:23 -0300

    fix bugs loops

--- Loops.py
+++ Loops.py
@@ -2,8 +2,8 @@ print("Números de 1 a 5 multiplicados por 2 com for")
 for numero in range(1, 6):
     print(numero * 2)
 
-print("\nNúmeros de 1 a 5 multiplicados por 2 con while")
+print("\nNúmeros de 1 a 5 multiplicados por 2 com while")
 contador = 1
 while contador <= 5:
     print(contador * 2)
-    contador =+ 1
+    contador += 1
