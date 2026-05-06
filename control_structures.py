6ca68e552439a326169ee3e42b4941362d975ffa
nyg7 <yuricantox@gmail.com>
2026-05-06 15:33:56 -0300

    control structures py

---
+++ control_structures.py
@@ -0,0 +1,16 @@
+idade = 17
+
+if idade < 18:
+    print("è menor de idade.")
+
+elif idade >= 18 and idade < 60:
+    print("É um adulto.")
+
+elif idade > 15:
+    print("Maior de 15 anos.")
+
+elif idade == 60:
+    print("Feliz Aniversário de 60 anos!")
+
+else:
+    print("É um Idoso.") 
